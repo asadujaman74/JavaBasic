8e367930f3a9324f4fbabd39093923bd2c90a656
asadujaman74 <asadujaman11@gmail.com>
2023-02-19 22:25:06 +0600

    asadu added new scanner function

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your Name : ");
+        String myName = input.nextLine();
+        System.out.println("Your Name is " + myName);
+        
     }
 }
