edfac44b418ee2b7e37892dee4308e1863e398fb
asadujaman74 <asadujaman11@gmail.com>
2023-02-19 22:43:44 +0600

    Sum added in this file

--- Input.java
+++ Input.java
@@ -4,6 +4,10 @@ public class Input{
         Scanner in = new Scanner(System.in);
         System.out.print("Enter your name: ");
         String name = in.nextLine();
-        System.out.printf("%s , Welcome to the world of java",name);
+        System.out.printf("%s , Welcome to the world of java \n",name);
+
+        int a = 5, b = 10;
+        int sum = a+b;
+        System.out.printf("the number is %d \n",  sum);
     }
 }
